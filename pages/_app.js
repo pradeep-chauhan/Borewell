cf5976850a10230def24ac77fb5a36714d2df5da
Jain-Daksh <daksh44jain@gmail.com>
2022-11-03 14:38:06 +0530

    homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+import '../design/common.css'
+import '../design/home.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/modules/Homepage/Homepage.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+
+const Navbar = () => {
+  return (
+    <section className='home'>
+      <div className='container'>
+        <nav class="navbar navbar-expand-lg  " id="navbar-2">
+          <div class="container-fluid">
+            <a class="navbar-brand" id="logo" href="#">Borewell</a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation" >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">TESTIMONIALS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+        <div className='container mt-5'>
+          <div className='row col-9 mt-5 mx-3'>
+            <h2 className='display-5 mx-3'>Trusted & Reliable</h2>
+            <h2 className='display-4'>Borewell Contractors</h2>
+          </div>
+        </div>
+        <div className='container mt-3 '>
+          <div class="btn btn-info button rounded-pill mx-5 ">Enquire Now</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar
