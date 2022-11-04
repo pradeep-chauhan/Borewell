c066003290d6e5b8a071fbe74352d9cac322c749
Jain-Daksh <daksh44jain@gmail.com>
2022-11-04 18:23:38 +0530

    gallery section changed

--- pages/modules/Gallery.jsx
+++ pages/modules/Gallery.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
-import Image1 from '../images/gallery1.jpeg'
-import Image2 from '../images/gallery2.webp'
-import Image3 from '../images/gallery3.jpeg'
-const Gallery = () => {
+import Image4 from '../images/aboutus.webp'
+import Image5 from '../images/services1.jpg'
+import Image6 from '../images/services2.webp'
+import Image7 from '../images/services3.webp'
+import Image8 from '../images/services4.webp'
+import Image9 from '../images/gallery4.jpg'
+const images = [
+  { picture: Image9 },
+  { picture: Image4 },
+  { picture: Image5 },
+  { picture: Image6 },
+  { picture: Image7 },
+  { picture: Image8 }
+]
+export default function Gallery() {
   return (
-    <section>
+    <>
       <div className='container'>
-      <div class="row pt-5">
+        <div className="row pt-5">
           <div class="col-12">
             <h3 class="text-uppercase mb-4 text-center heading">Gallery</h3>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12'>
-            <Image src={Image1} className='img-fluid img-curve' />
-          </div>
-          <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12'>
-            <Image src={Image2} className='img-fluid img-curve' />
-          </div>
-          <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12'>
-            <Image src={Image3} className='img-fluid img-curve' />
+          <div className='row'>
+          {
+                images.map(({ picture }) => (
+                  <div className="col-sm-6 col-xs-6 col-lg-4 col-xl-4 col-md-6 mt-3">
+                    <Image key={picture} src={picture} className="gallery-images card-img-top img-fluid" />
+                  </div>
+                ))
+              }
           </div>
         </div>
       </div>
-    </section>
+    </>
   )
 }
-
-export default Gallery
--- pages/modules/Testimonial.jsx
+++ pages/modules/Testimonial.jsx
@@ -1,142 +1,3 @@
-// import React from 'react'
-// import Image from 'next/image'
-// import Image1 from '../images/people1.jpeg'
-// import Image2 from '../images/people2.webp'
-// import Image3 from '../images/people3.webp'
-// function Testimonial() {
-//     return (
-//         <>
-//             <div>
-//                 <div className="container">
-//                     <div class="row pt-5">
-//                         <div class="col-12">
-//                             <h3 class="text-uppercase mb-4 text-center heading">Testimonial</h3>
-//                         </div>
-//                     </div>
-//                     <div className="row">
-//                         <div className="col-xl-4 col-lg-4 col-md-6 col-xs-12 col-sm-12 d-flex justify-content-center">
-//                             <div className="card card-testimonial d-flex justify-content-center">
-//                                 <div className="face front-face">
-//                                     <Image src={Image1} className="profile" />
-//                                     <div className="pt-3 text-uppercase name">
-//                                         Robert Garrison
-//                                     </div>
-//                                     <div className="designation">Android Developer</div>
-//                                 </div>
-//                                 <div className="face back-face">
-//                                     <span className="fas fa-quote-left"></span>
-//                                     <div className="testimonial">
-//                                         I made bacck the purchase price in just 48 hours! Thank you for making it pain less,
-//                                         pleasant.
-//                                         The service was execellent. I will refer everyone I know.
-//                                     </div>
-//                                     <span className="fas fa-quote-right"></span>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className="col-xl-4 col-lg-4 col-md-6 col-xs-12 col-sm-12 d-flex justify-content-center">
-//                             <div className="card card-testimonial">
-//                                 <div className="face front-face">
-//                                     <Image src={Image2} className="profile" />
-//                                     <div className="pt-3 text-uppercase name">
-//                                         Jeffery Kennan
-//                                     </div>
-//                                     <div className="designation">Full Stack Developer</div>
-//                                 </div>
-//                                 <div className="face back-face">
-//                                     <span className="fas fa-quote-left"></span>
-//                                     <div className="testimonial">
-//                                         Really good, you have saved our business! I made bacck the purchase price in just 48 hours!
-//                                         man, this thing is getting better and better as I learn more about it.
-//                                     </div>
-//                                     <span className="fas fa-quote-right"></span>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className="col-xl-4 col-lg-4 col-md-6 col-xs-12 col-sm-12 d-flex justify-content-center">
-//                             <div className="card card-testimonial">
-//                                 <div className="face front-face">
-//                                     <Image src={Image3} className="profile" />
-//                                     <div className="pt-3 text-uppercase name">
-//                                         Issac Maxwell
-//                                     </div>
-//                                     <div className="designation">Finance Director</div>
-//                                 </div>
-//                                 <div className="face back-face">
-//                                     <span className="fas fa-quote-left"></span>
-//                                     <div className="testimonial">
-//                                         Account keeper is the most valuable business research we have EVER purchased. Without
-//                                         electrician, we would ahave gone bankrupt by now.
-//                                     </div>
-//                                     <span className="fas fa-quote-right"></span>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Testimonial
-
-
-// import React from 'react'
-
-// const Testimonial = () => {
-//     return (
-//         <div><div class="bg-dark p-5 mx-auto mt-3 mb-3">
-//             <div id="carouselTestimonial" class="carousel carousel-testimonial slide" data-ride="carousel">
-//                 <ol class="carousel-indicators">
-//                     <li data-target="#carouselTestimonial" data-slide-to="0" class="active"></li>
-//                     <li data-target="#carouselTestimonial" data-slide-to="1"></li>
-//                     <li data-target="#carouselTestimonial" data-slide-to="2"></li>
-//                 </ol>
-//                 <div class="carousel-inner">
-//                     <div class="carousel-item text-center active">
-//                         <div class="carousel-testimonial-img p-1 border rounded-circle m-auto">
-
-//                         </div>
-//                         <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">SARA A. MORAN</strong></h5>
-//                         <h6 class="text-dark m-0">Web Developer</h6>
-//                         <p class="m-0 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, consectetur adipiscing elit varius quam at, luctus dui. Mauris magna metus consectetur adipiscing elit.</p>
-//                     </div>
-//                     <div class="carousel-item text-center">
-//                         <div class="carousel-testimonial-img p-1 border rounded-circle m-auto">
-
-//                         </div>
-//                         <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">STEVE FONSI</strong></h5>
-//                         <h6 class="text-dark m-0">Web Designer</h6>
-//                         <p class="m-0 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, consectetur adip varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel.</p>
-//                     </div>
-//                     <div class="carousel-item text-center">
-//                         <div class="carousel-testimonial-img p-1 border rounded-circle m-auto">
-
-//                         </div>
-//                         <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">SANDRA M. GARCIA</strong></h5>
-//                         <h6 class="text-dark m-0">Seo Analyst</h6>
-//                         <p class="m-0 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, consectetur adipiscing elit consectetur adipiscing elit luctus dui. Mauris magna metus.</p>
-//                     </div>
-//                 </div>
-//                 <a class="carousel-control-prev" href="#carouselTestimonial" role="button" data-slide="prev">
-//                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//                     <span class="sr-only">Previous</span>
-//                 </a>
-//                 <a class="carousel-control-next" href="#carouselTestimonial" role="button" data-slide="next">
-//                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//                     <span class="sr-only">Next</span>
-//                 </a>
-//             </div>
-//         </div>
-//         </div>
-//     )
-// }
-
-// export default Testimonial
-
-
-
 import React from 'react'
 import { ArrowLeft, ArrowRight, Person } from 'react-bootstrap-icons';
 import Slider from "react-slick";
@@ -145,11 +6,11 @@ import "slick-carousel/slick/slick-theme.css";
 import Image from 'next/image'
 import Image1 from '../images/people1.jpeg'
 import Image2 from '../images/people2.webp'
- import Image3 from '../images/people3.webp'
+import Image3 from '../images/people3.webp'
 const data = [
     {
         id: 1,
-        name: "Lance Jarvis",
+        name: "Juliet Jarvis",
         avatar: Image1,
         message:
             "We have been using product for last one year, and I have to say that it has transformed the way we do business. Thank you for awesome service.",
@@ -158,7 +19,7 @@ const data = [
     },
     {
         id: 2,
-        name: "Juliet Wright",
+       name: "Michael D.",
         avatar: Image2,
         message:
             "It has changed the way I used the website. Product lets you create anything you envision and it does it so easy and flawless. I can't imagine not working with the service.",
@@ -167,7 +28,7 @@ const data = [
     },
     {
         id: 3,
-        name: "Michael D.",
+        name: "Juliet Wright",
         avatar: Image3,
         message:
             "Insights from service drove 30% lift in our conversions, this product service is essential for your company growth strategy.",
@@ -195,71 +56,12 @@ const NextBtn = (props) => {
         </div>
     );
 };
-//   const Testimonial = () => {
-//     return (
-//       <div
-//         className="testimonial"
-//         style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
-//       >
-//         <div style={{ width: "50%", textAlign: "center" }}>
-//           <h1 style={{ marginBottom: 20 }}>TESTIMONIALS</h1>
-//           <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
-//             <Card img={Image1} />
-//             <Card img="https://www.tutorialrepublic.com/examples/images/clients/2.jpg" />
-//             <Card img="https://www.tutorialrepublic.com/examples/images/clients/3.jpg" />
-
-//           </Slider>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   const Card = ({ img }) => {
-//     return (
-//       <div
-//         style={{
-//           display: "flex",
-//           alignItems: "center",
-//           flexDirection: "column",
-//           textAlign: "center",
-//           color: "gray",
-//         }}
-//       >
-//         <Person
-//           imgProps={{ style: { borderRadius: "50%" } }}
-//           src={img}
-//           style={{
-//             width: 120,
-//             height: 120,
-//             border: "1px solid lightgray",
-//             padding: 7,
-//             marginBottom: 20,
-//           }}
-//         />
-//         <p>
-//           Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia.
-//           Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan
-//           tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt
-//           fringilla massa. Etiam hendrerit dolor eget rutrum
-//         </p>
-//         <p style={{ fontStyle: "italic", marginTop: 25 }}>
-//           <span style={{ fontWeight: 500, color: "green" }}>PAULA WILSON</span> ,
-//           Media Analyst
-//         </p>
-//       </div>
-//     );
-//   };
-
-
-
 const Testimonial = () => {
     return (
         <section>
             <div className='container'>
-             
                 <div className='testimonail pt-5'>
                     <h1 className='text-uppercase mb-4 text-center heading' >TESTIMONIALS</h1>
-
                     <div className='row d-flex-center '>
                         <div className='text-center d-flex justify-content-center col-12 mb-5'>
                             <div className='card ' >
@@ -278,11 +80,7 @@ const Testimonial = () => {
                             </div>
                         </div>
                     </div>
-
-
-
                 </div>
-               
             </div>
         </section>
     )
