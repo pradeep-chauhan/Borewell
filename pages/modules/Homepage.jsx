c533b2d7c375e8d80c6428660965af52cf6f7ff3
Jain-Daksh <daksh44jain@gmail.com>
2022-11-05 10:56:11 +0530

    navbar and homepage

--- pages/modules/Homepage.jsx
+++ pages/modules/Homepage.jsx
@@ -1,65 +1,114 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
+// import React from 'react'
+// import { Nav } from 'react-bootstrap'
 
-const Navbar = () => {
-  return (
-    <section className='home'>
-      <div className='container'>
-        <nav class="navbar navbar-expand-lg  " id="navbar-2">
-          <div class="container-fluid">
-            <a class="navbar-brand" id="logo" href="#">Borewell</a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation" >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-              <ul class="navbar-nav ">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">SERVICES</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">ABOUT US</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">GALLERY</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">VIDEOS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">TESTIMONIALS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">CONTACT</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <div className='container mt-5'>
-          <div className='row col-9 mt-5 mx-3'>
-            <h2 className='display-5 mx-3'>Trusted & Reliable</h2>
-            <h2 className='display-4'>Borewell Contractors</h2>
-          </div>
-        </div>
-        <div className='container mt-3 '>
-          <div class="btn btn-info button rounded-pill mx-5 ">Enquire Now</div>
-        </div>
-      </div>
-    </section>
-  )
-}
+// const Navbar = () => {
+//   return (
+//     <section className='home'>
+//       <div className='container'>
+//         {/* <nav class="navbar navbar-expand-lg  " id="navbar-2">
+//           <div class="container-fluid">
+//             <a class="navbar-brand" id="logo" href="#">Borewell</a>
+//             <button
+//               class="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation" >
+//               <span class="navbar-toggler-icon"></span>
+//             </button>
+//             <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+//               <ul class="navbar-nav ">
+//                 <li class="nav-item">
+//                   <a class="nav-link active" aria-current="page" href="#">HOME</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">SERVICES</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">ABOUT US</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">GALLERY</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">VIDEOS</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">TESTIMONIALS</a>
+//                 </li>
+//                 <li class="nav-item">
+//                   <a class="nav-link" href="#">CONTACT</a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav> */}
+//         <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar-2">
+//           <div class="container-fluid">
+//             <a class="navbar-brand" id="logo" href="#">KID<span id="logo2">care</span></a>
+//             <button
+//               class="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation" >
+//               <span class="navbar-toggler-icon"></span>
+//             </button>
+//             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+// <ul class="navbar-nav ">
+//               <li class="nav-item">
+//                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Class</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Teacher</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Blog</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Event</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Page</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Contact</a>
+//               </li>
+//             </ul>
 
-export default Navbar
+//           </div>
+//       </div>
+//     </nav>
+//         {/* <div className='container mt-5'>
+//           <div className='row col-9 text-center mt-5 mx-4'>
+//             <h2 className='display-5 mx-3'>Trusted & Reliable</h2>
+//             <h2 className='display-4'>Borewell Contractors</h2>
+//           </div>
+//         </div>
+//         <div className='container text-center mt-3 '>
+//           <div class="btn btn-info button rounded-pill ">Enquire Now</div>
+//         </div> */}
+//   <div className="container">
+//     <div className="row mt-5 pt-5">
+//       <div className="col-8 mt-5 text-center">
+//         <h1 className='display-5'>Trusted and reliable</h1>
+//         <h2 className='display-4'>Borewell Contractors</h2>
+//       </div>
+//     </div>
+//   </div>
+//       </div >
+//     </section >
+//   )
+// }
+
+// export default Navbar
 
 
 // import React from 'react'
@@ -205,7 +254,7 @@ export default Navbar
 //         </section> */}
 //         <div id="carouselExampleControls" class="carousel slide" data-bs-interval="5000" data-bs-ride="carousel">
 //           <div class="carousel-inner">
-          
+
 //             <div class="carousel-item active">
 //               <div class="home-1"
 //               >
@@ -259,3 +308,127 @@ export default Navbar
 
 
 
+// import React from 'react'
+
+// const Homepage = () => {
+//   return (
+//     <section>
+//       <div className='container'>
+
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Homepage
+
+
+
+{/* <nav class="navbar navbar-expand-lg  " id="navbar-2">
+           <div class="container-fluid">
+             <a class="navbar-brand" id="logo" href="#">Borewell</a>
+             <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation" >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">TESTIMONIALS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav> */}
+
+
+
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+
+const Navbar = () => {
+  return (
+    <section className='home'>
+      <div className='container'>
+        <nav class="navbar navbar-expand-lg " id="navbar-2">
+          <div class="container-fluid">
+            <a class="navbar-brand" id="logo" href="#">Borewell</a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation" >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">TESTIMONIALS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row mt-5 pt-5">
+            <div className="col-8 mt-5">
+              <h1 className='display-5'>Trusted and reliable</h1>
+              <h2 className='display-4'>Borewell Contractors</h2>
+            </div>
+            <div className='container  mt-3 '>
+              <div class="btn btn-info button rounded-pill ">Enquire Now</div>
+            </div>
+          </div>
+        </div>
+      </div >
+    </section >
+  )
+}
+
+export default Navbar
