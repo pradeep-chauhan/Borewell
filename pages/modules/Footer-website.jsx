14e0df524d82af92ab7175d34f743f599988fcb7
Jain-Daksh <daksh44jain@gmail.com>
2022-11-05 15:49:27 +0530

    about us section changed

--- pages/modules/Footer-website.jsx
+++ pages/modules/Footer-website.jsx
@@ -8,7 +8,7 @@ function FooterWebsite() {
         <div className="row  pt-5">
         <hr></hr>
           <div className="col-12 col-md-8 col-sm-12 col-xs-12 mt-3 mb-2 ">
-            © {year} BoreWell. All Rights Reserved
+            © {year} Mahalaxmi Borewell. All Rights Reserved
           </div>
           <div className="col-12 col-md-4 col-sm-12 col-xs-12 mt-3 mb-2  ">
             <p>Design and Developed By <strong> <a href="https://alvrio.io/">Alvrio Labs</a></strong></p>
--- pages/modules/Homepage.jsx
+++ pages/modules/Homepage.jsx
@@ -1,61 +1,69 @@
 import React from 'react'
+import { useState } from 'react';
 const Navbar = () => {
-  return (
-    <section className='home'>
-      <div className='container'>
-        <nav class="navbar navbar-expand-lg " id="navbar-2">
-          <div class="container-fluid">
-            <a class="navbar-brand" id="logo" href="#">Borewell</a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation" >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-              <ul class="navbar-nav ">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">SERVICES</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">ABOUT US</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">GALLERY</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">VIDEOS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">TESTIMONIALS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">CONTACT</a>
-                </li>
-              </ul>
+  const [isScrolled, setIsScrolled] = useState(false);
 
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <section className='home' >
+      <div className='className={isScrolled ? "navbar scrolled" : "navbar"}'>
+        <div className='container'>
+          <nav class="navbar navbar-expand-lg " id="navbar-2">
+            <div class="container-fluid">
+              <a class="navbar-brand" id="logo" href="#">Mahalaxmi Borewell</a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation" >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav ">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">SERVICES</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">ABOUT US</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">GALLERY</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">VIDEOS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">TESTIMONIALS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row mt-5 pt-5">
+              <div className="col-8 mt-5">
+                <h1 className='display-5'>Trusted and reliable</h1>
+                <h2 className='display-4'>Borewell Contractors</h2>
+              </div>
+              <div className='container  mt-3 '>
+                <div class="btn  button rounded-pill ">Enquire Now</div>
+              </div>
             </div>
           </div>
-        </nav>
-        <div className="container">
-          <div className="row mt-5 pt-5">
-            <div className="col-8 mt-5">
-              <h1 className='display-5'>Trusted and reliable</h1>
-              <h2 className='display-4'>Borewell Contractors</h2>
-            </div>
-            <div className='container  mt-3 '>
-              <div class="btn  button rounded-pill ">Enquire Now</div>
-            </div>
-          </div>
-        </div>
-      </div >
+        </div ></div>
     </section >
   )
 }
