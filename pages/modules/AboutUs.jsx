de3df017158a8e7a64351beeb3735f5abce5b99e
Jain-Daksh <daksh44jain@gmail.com>
2022-11-05 16:22:50 +0530

    about us

--- pages/modules/AboutUs.jsx
+++ pages/modules/AboutUs.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import Image1 from '../images/aboutus.webp'
+// import Image1 from '../images/Construction-pana.svg'
+import Image1 from '../images/aboutus1.jpg'
+
 const AboutUs = () => {
   return (
     <div>
       <div className='container pt-5'>
         <div className='row'>
-          <div className='col pt-5 mt-5'>
-            <h3 class="text-uppercase mb-4 text-center mt-5 heading">About Us</h3>
-            <p className='paragraph'>We, Vision Borewell Services, situated at Udaipur, Rajasthan are recognized as one of the most reliable multi-disciplinary drilling contractors. We are known for our trustworthy services of soil testing, borewell, drilling & industrial drillings, etc. Our company is backed by trained and devoted experts that keep updating themselves with all the latest industrial technologies.</p>
+
+          <div className='col-xl-6 col-md-6 col-lg-6 col-sm-12 col-xs-12 aboutus-image'>
+            <div className="aboutus-img">
+              <Image src={Image1} className='img-fluid img-curve' />
+            </div>
           </div>
-          
-          <div className='col'>
-            <Image src={Image1} className='img-fluid img-curve' />
+          <div className='col-xl-6 col-md-6 col-lg-6 col-sm-12 col-xs-12 '>
+            <h3 class="text-uppercase mb-4 text-center mt-2 heading">About Us</h3>
+            <p className='paragraph'>We, Mahalaxmi Borewell, situated at Udaipur, Rajasthan are recognized as one of the most reliable multi-disciplinary drilling contractors. We are known for our trustworthy services of soil testing, borewell, drilling and industrial drillings, etc. Our company is backed by trained and devoted experts that keep updating themselves with all the latest industrial technologies.</p>
           </div>
         </div>
       </div>
