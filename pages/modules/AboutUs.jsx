dced5c81a83b7268501b7619501d59ab13e774cd
Jain-Daksh <daksh44jain@gmail.com>
2022-11-05 16:16:48 +0530

    about us section changed

--- pages/modules/AboutUs.jsx
+++ pages/modules/AboutUs.jsx
@@ -10,6 +10,7 @@ const AboutUs = () => {
             <h3 class="text-uppercase mb-4 text-center mt-5 heading">About Us</h3>
             <p className='paragraph'>We, Vision Borewell Services, situated at Udaipur, Rajasthan are recognized as one of the most reliable multi-disciplinary drilling contractors. We are known for our trustworthy services of soil testing, borewell, drilling & industrial drillings, etc. Our company is backed by trained and devoted experts that keep updating themselves with all the latest industrial technologies.</p>
           </div>
+          
           <div className='col'>
             <Image src={Image1} className='img-fluid img-curve' />
           </div>
