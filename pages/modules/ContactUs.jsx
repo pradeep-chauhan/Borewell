f821b8b679e796bf054e90b9747e0e2102883135
Jain-Daksh <daksh44jain@gmail.com>
2022-11-08 12:28:21 +0530

    contact us

--- pages/modules/ContactUs.jsx
+++ pages/modules/ContactUs.jsx
@@ -207,20 +207,135 @@
 // export default ContactUs
 
 
+// import React from 'react'
+// import { Map, Phone, Envelope, Mailbox } from 'react-bootstrap-icons';
+// import { Facebook, Instagram, Whatsapp, Twitter } from 'react-bootstrap-icons'
+// const ContactUs = () => {
+//   return (
+//     <section>
+//       <div className="container">
+//         {/* <div className='row pt-5'>
+//           <div className='col-lg-6 col-xl-6 col-md-4 col-sm-12 col-xs-12'>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Map className="fa fa-map-marker"></Map>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p>648, Zinc Smelter, Debari, near manisha medical, Udaipur, Rajasthan 313024</p>
+//               </div>
+//             </div>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Phone className="fa fa-phone contactIcon"></Phone>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p><a href="tel:+919928845025" className="">+91-9928845025</a></p>
+//               </div>
+//             </div>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Phone className="fa fa-phone contactIcon"></Phone>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p><a href="tel:+919782525025" className="">+91-9782525025</a></p>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="col-lg-6 col-xl-6 col-md-8 col-sm-12 col-xs-12">
+//             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14510.127844554047!2d73.8154725!3d24.6053439!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3967e7045afd7ce9%3A0xd7bdb60d6cbac5d8!2sAlvrio%20Labs!5e0!3m2!1sen!2sin!4v1667657045373!5m2!1sen!2sin" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+//           </div>
+//         </div> */}
+
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default ContactUs
+
+
+// import React from 'react'
+// import { Clock, Envelope, Geo, Map, Phone, Watch } from 'react-bootstrap-icons'
+
+// const ContactUs = () => {
+//   return (
+//     <section>
+//       <div className='container'>
+//         <div className="row mt-5">
+//           <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+//             <h3 className='heading mb-3 mt-5 service'>Contact Us</h3>
+//             <p></p>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Geo className="fa fa-map-marker"></Geo>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p>648, Zinc Smelter, Debari, near manisha medical, Udaipur, Rajasthan 313024</p>
+//               </div>
+//             </div>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Phone className="fa fa-phone contactIcon"></Phone>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p><a href="tel:+919782525025" className="">+91-9782525025</a></p>
+//               </div>
+//             </div>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Phone className="fa fa-phone contactIcon"></Phone>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//               <p><a href="tel:+919928845025" className="">+91-9928845025</a></p>
+//               </div>
+//             </div>
+//             <div className='row'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//               <Envelope> </Envelope>
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//               <p><a href="mailto:abc.com" className=""> xyz@gmail.com </a></p>
+//               </div>
+//             </div>
+//             <div className='row mb-5'>
+//               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+//                 <Clock />
+//               </div>
+//               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+//                 <p>Mon-Sat: 10AM - 6PM</p>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+//             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14510.127844554047!2d73.8154725!3d24.6053439!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3967e7045afd7ce9%3A0xd7bdb60d6cbac5d8!2sAlvrio%20Labs!5e0!3m2!1sen!2sin!4v1667657045373!5m2!1sen!2sin" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+
+//   )
+// }
+
+// export default ContactUs
+
+
 import React from 'react'
-import { Map, Phone, Envelope } from 'react-bootstrap-icons';
-import { Facebook, Instagram, Whatsapp, Twitter } from 'react-bootstrap-icons'
+import { Clock, Envelope, Geo, Phone } from 'react-bootstrap-icons'
+
 const ContactUs = () => {
   return (
-    <section>
+    <div>
       <div className="container">
-        <div className='row pt-5'>
-          <div className='col-lg-6 col-xl-6 col-md-4 col-sm-12 col-xs-12'>
-            <div className='row'>
+        <div className="row mt-5">
+          <div className="col">
+            <h3 className='heading mb-3 mt-5 service'>Contact Us</h3>
+            <p >We Will love to here from you</p>
+
+            <div className='row mt-3'>
               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
-                <Map className="fa fa-map-marker"></Map>
+                <Geo className="fa fa-map-marker"></Geo>
               </div>
-              <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+              <div className='col-md-10 col-xl-10 col-lg-10 col-sm-11'>
                 <p>648, Zinc Smelter, Debari, near manisha medical, Udaipur, Rajasthan 313024</p>
               </div>
             </div>
@@ -229,24 +344,40 @@ const ContactUs = () => {
                 <Phone className="fa fa-phone contactIcon"></Phone>
               </div>
               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
-                <p><a href="tel:+91xxxxxxxxxx" className="">+91-1234567890</a></p>
+                <p><a href="tel:+919928845025" className="">+91-9928845025</a></p>
               </div>
             </div>
             <div className='row'>
               <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
-                <Envelope> </Envelope>
+                <Phone className="fa fa-phone contactIcon"></Phone>
+              </div>
+              <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+                <p><a href="tel:+919928845025" className="">+91-9928845025</a></p>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+                <Envelope className="fa"> </Envelope>
               </div>
               <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
                 <p><a href="mailto:abc.com" className=""> xyz@gmail.com </a></p>
               </div>
             </div>
+            <div className='row mb-5'>
+              <div className='col-md-1 col-xl-1 col-lg-1 col-sm-1'>
+                <Clock className="fa"/>
+              </div>
+              <div className='col-md-11 col-xl-11 col-lg-11 col-sm-11'>
+                <p>Mon-Sat: 10AM - 6PM</p>
+              </div>
+            </div>
           </div>
-          <div className="col-lg-6 col-xl-6 col-md-8 col-sm-12 col-xs-12">
+          <div className="col">
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14510.127844554047!2d73.8154725!3d24.6053439!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3967e7045afd7ce9%3A0xd7bdb60d6cbac5d8!2sAlvrio%20Labs!5e0!3m2!1sen!2sin!4v1667657045373!5m2!1sen!2sin" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
