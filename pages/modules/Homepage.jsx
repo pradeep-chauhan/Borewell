433e44d9828ff1fffbf781ad2f2c43b19ae1653b
Jain-Daksh <daksh44jain@gmail.com>
2022-11-09 12:04:48 +0530

    navbar fixed

--- pages/modules/Homepage.jsx
+++ pages/modules/Homepage.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Image1 from '../images/whatsapp.png'
 const Homepage = () => {
   return (
-    <section class="home">
+    <section >
       <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-interval="10000">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
@@ -15,25 +15,20 @@ const Homepage = () => {
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="image-1 homepage-image-2">
-              <div class="card move-text opacity-75 card-move" >
-                <div class="card-body">
+              <div class=" move-text  card-move" >
                   <h5 class="card-title fs-2">BOREWELLS DRILLING EQUIPMENTS &<br />
                     CONTRACTOR</h5>
                   <div class="btn  button rounded-pill text-white px-4">Enquire Now</div>
-
-                </div>
               </div>
             </div>
           </div>
           <div class="carousel-item">
             <div class="image-1 homepage-image-1">
-              <div class="card move-text opacity-75 card-move">
-                <div class="card-body">
-                  <h5 class="card-title fs-2">BOREWELLS DRILLING EQUIPMENTS &<br />
-                    CONTRACTOR</h5>
-                  <div class="btn  button rounded-pill text-white px-4">Enquire Now</div>
-                </div>
-              </div>
+            <div class=" move-text  card-move" >
+                <h5 class="card-title fs-2">BOREWELLS DRILLING EQUIPMENTS &<br />
+                  CONTRACTOR</h5>
+                <div class="btn  button rounded-pill text-white px-4">Enquire Now</div>
+            </div>
             </div>
           </div>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
--- pages/modules/Navbar.jsx
+++ pages/modules/Navbar.jsx
@@ -5,37 +5,42 @@ const Navbar = () => {
   return (
     <section >
       <div class="container-fluid ">
-        <nav class="navbar navbar-expand-lg fixed-top bg-transparent mx-5" 
-          >
-          <a class="navbar-brand text-black" href="#">Mahalaxmi Borewell</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">HOME</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">SERVICES</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">ABOUT US</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">GALLERY</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">VIDEOS</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">TESTIMONIALS</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">CONTACT</a>
-              </li>
-            </ul>
+        <nav class="navbar navbar-expand-lg fixed-top bg-transparent mx-5" id="navbar-2">
+          <div class="container-fluid">
+            <a class="navbar-brand" id="logo" href="#">Mahalaxmi Borewell</a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation" >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">TESTIMONIALS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT</a>
+                </li>
+              </ul>
+
+            </div>
           </div>
         </nav>
       </div >
